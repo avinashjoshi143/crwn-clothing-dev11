0dbc5aa45a02c4ee3ea676bdd4f74d72c0a76156
Avinash Joshi <AvinashJoshi7@outlook.com>
2021-07-03 17:58:11 +0530

    replaced the component state into redux wherever its needed and created seperate component collection overview for shop page

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
+import './shoppage.styles.scss';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
-import SHOP_DATA from './shop.data';
+const ShopPage = () => (
+    <div className="shop-page">
+        <CollectionOverview /> 
+    </div>
+)
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-class ShopPage extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            collections: SHOP_DATA
-        } 
-    }
-
-    render() {
-        const {collections} = this.state
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id,...otherCollectionProps}) => 
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    )
-                }
-            </div>
-        )
-    }
-}
 
 export default ShopPage;
